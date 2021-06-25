eba84eeb44c357ac1b36301b33b5718afc6c3b63
bakerv <80018640+bakerv@users.noreply.github.com>
2021-06-24 21:15:35 -0500

    script functional, corrected syntax errors

--- static/UFO-level-1/table.js
+++ static/UFO-level-1/table.js
@@ -1,12 +1,12 @@
 
 function convertData(saveLocation, dataSource){
     // Loop through each Object in the data array
-   dataSource.forEach(ufObject =>{
+   dataSource.forEach((ufObject) => {
        // insert a new row for each Object
        let row = saveLocation.insertRow();
 
        // Loop through all keys in each Object
-       ufObject.forEach(key => {
+       Object.entries(ufObject).forEach(([key,value]) => {
            // Create and fill cells with values from the Objects keys
           let cell = row.insertCell();
           let contents = document.createTextNode(ufObject[key]);
@@ -15,16 +15,16 @@ function convertData(saveLocation, dataSource){
    })
 
    // After creating the table body, add the table heads
-   let row = saveLocation.createTHead().insertRow();
-   dataSource[0].forEach(key =>{
+   let rowHeader = saveLocation.createTHead().insertRow();
+   Object.entries(dataSource[0]).forEach(([key, value]) =>{
        let tableHeader = document.createElement("th");
-       let contents = document.CreateTextNode(key);
-       th.appendChild(contents);
-       row.appendChild(tableHeader);
+       let contents = document.createTextNode(key);
+       tableHeader.appendChild(contents);
+       rowHeader.appendChild(tableHeader);
    })
 }
 // Inspiration for this function came from Valentino Gagliardi www.valentiog.com
 
 let saveLocation = document.querySelector("table");
 let dataSource = data;
-//convertData(saveLocation, dataSource);
+convertData(saveLocation, dataSource);
