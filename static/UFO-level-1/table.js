64f80df0c104fe5b96ebd202bf0890f6aa796d4d
bakerv <80018640+bakerv@users.noreply.github.com>
2021-06-29 13:44:03 -0500

    used the d3 library to create the table with much cleaner code

--- static/UFO-level-1/table.js
+++ static/UFO-level-1/table.js
@@ -1,30 +1,52 @@
 
-function convertData(saveLocation, dataSource){
-    // Loop through each Object in the data array
-   dataSource.forEach((ufObject) => {
-       // insert a new row for each Object
-       let row = saveLocation.insertRow();
+// function convertData(saveLocation, dataSource){
+//     // Loop through each Object in the data array
+//    dataSource.forEach((ufObject) => {
+//        // insert a new row for each Object
+//        let row = saveLocation.insertRow();
 
-       // Loop through all keys in each Object
-       Object.entries(ufObject).forEach(([key,value]) => {
-           // Create and fill cells with values from the Objects keys
-          let cell = row.insertCell();
-          let contents = document.createTextNode(ufObject[key]);
-          cell.appendChild(contents);
-       })
-   })
+//        // Loop through all keys in each Object
+//        Object.entries(ufObject).forEach(([key,value]) => {
+//            // Create and fill cells with values from the Objects keys
+//           let cell = row.insertCell();
+//           let contents = document.createTextNode(ufObject[key]);
+//           cell.appendChild(contents);
+//        })
+//    })
 
-   // After creating the table body, add the table heads
-   let rowHeader = saveLocation.createTHead().insertRow();
-   Object.entries(dataSource[0]).forEach(([key, value]) =>{
-       let tableHeader = document.createElement("th");
-       let contents = document.createTextNode(key);
-       tableHeader.appendChild(contents);
-       rowHeader.appendChild(tableHeader);
-   })
-}
-// Inspiration for this function came from Valentino Gagliardi www.valentiog.com
+//    // After creating the table body, add the table heads
+//    let rowHeader = saveLocation.createTHead().insertRow();
+//    Object.entries(dataSource[0]).forEach(([key, value]) =>{
+//        let tableHeader = document.createElement("th");
+//        let contents = document.createTextNode(key);
+//        tableHeader.appendChild(contents);
+//        rowHeader.appendChild(tableHeader);
+//    })
+// }
 
-let saveLocation = document.querySelector("table");
-let dataSource = data;
-convertData(saveLocation, dataSource);
+function displayData(data){
+    let tbody = d3.select("tbody")
+    let thead = d3.select("thead")
+
+    data.forEach((ufoReport) => {
+        let row = tbody.append("tr");
+        Object.entries(ufoReport).forEach(([key,value]) => {
+            let cell = row.append("td");
+            cell.text(value);
+        })
+    })
+
+    let header = thead.append("tr");
+    Object.entries(data[0]).forEach(([key,value]) => {
+        let contents = header.append("th");
+        contents.text(key);
+    })
+        }
+
+displayData(data)
+// function dateFilter(dataSource){
+//     // return = 
+// }
+// let saveLocation = document.querySelector("table");
+// let dataSource = data;
+// convertData(saveLocation, dataSource)
